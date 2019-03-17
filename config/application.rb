526649ab674ede5985d4743869d6596465cb53d7
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-03-17 11:38:52 +0900

    Fine setting commit

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module TechpitJob
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Asia/Tokyo' #この行追加
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja #この行を追加
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
